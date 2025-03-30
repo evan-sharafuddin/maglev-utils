d3694c0e8acf7ae7c9eb4016f21da613da102be3
Evan Sharafuddin <evansharafuddin@wustl.edu>
2025-03-30 19:50:24 +0100

    cleaned up the repo. Added testing and calibration scripts to their own directory

--- sensor_tests/hall_effect.py
+++
@@ -1,9 +0,0 @@
-import mcp3008
-import RPi.GPIO as GPIO
-
-adc=mcp3008.MCP3008(Vdd_hi=True)
-while(True):
-    out=adc.read(7)
-    print("ADC reading")
-    print(out)
-        
--- test_controller.py
+++
@@ -1,7 +0,0 @@
-from controller import Controller 
-
-control = Controller(window_size=3)
-
-control.control(chan = 7)
-
-
